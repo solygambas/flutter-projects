ab28c894b80fe0b03bf40ff37f4e881b159c4f43
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-22 07:48:55 +0100

    add custom colors and fonts

--- playground/02-basic-app/README.md
+++ playground/02-basic-app/README.md
@@ -4,7 +4,9 @@ A quick intro to Flutter.
 
 ## Features
 
-- creating a Flutter app and understanding file structure
+- creating a Flutter app and understanding its file structure.
 - building a layout with Scaffold.
+- adding custom colors and fonts.
+- rendering stateless widgets.
 
 Based on [Flutter Tutorial for Beginners](https://www.youtube.com/watch?v=1ukSR1GRtMU&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ) by Shaun Pelling - The Net Ninja (2019).
--- playground/02-basic-app/lib/main.dart
+++ playground/02-basic-app/lib/main.dart
@@ -2,19 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
 appBar: AppBar(
   title: const Text('My first app'),
   centerTitle: true,
+  backgroundColor: Colors.red[600],
 ),
-body: const Center(
-  child: Text('Hello ninjas'),
+body: Center(
+  child: Text('Hello ninjas', style: TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 2.0,
+    color: Colors.grey[600],
+    fontFamily: 'IndieFlower',
+  )),
 ),
 floatingActionButton: FloatingActionButton(
   child: const Text('Click'),
   onPressed: (){},
+  backgroundColor: Colors.red[600],
 ),
-    ),
-    ),
-  );
+    );
+  }
 }
