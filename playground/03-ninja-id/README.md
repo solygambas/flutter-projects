072bc91bf2ae0800689f3ed7d80570834e88c536
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-23 08:38:20 +0100

    add stater

--- playground/03-ninja-id/README.md
+++ playground/03-ninja-id/README.md
@@ -5,5 +5,6 @@ A simple project to display ID cards.
 ## Features
 
 - building an ID card with SizedBox and CircleAvatar widgets.
+- handling state with stateful widgets.
 
 Based on [Flutter Tutorial for Beginners](https://www.youtube.com/watch?v=1ukSR1GRtMU&list=PL4cUxeGkcC9jLYyp2Aoh6hcWuxFDX6PBJ) by Shaun Pelling - The Net Ninja (2019).
--- playground/03-ninja-id/lib/main.dart
+++ playground/03-ninja-id/lib/main.dart
@@ -6,92 +6,94 @@ void main() {
   ));
 }
 
-class NinjaCard extends StatelessWidget {
-  const NinjaCard({ Key? key }) : super(key: key);
+class NinjaCard extends StatefulWidget {
+  const NinjaCard({Key? key}) : super(key: key);
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: const Text('Ninja ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/thumb.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 90.0,
-              color: Colors.grey[800],
-            ),
-            const Text(
-              'NAME', 
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              )
-            ),
-            const SizedBox(height: 10.0),
-            Text(
-              'Chun-Li', 
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              )
-            ),
-            const SizedBox(height: 30.0),
-            const Text(
-              'CURRENT NINJA LEVEL', 
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              )
-            ),
-            const SizedBox(height: 10.0),
-            Text(
-              '8', 
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              )
-            ),
-            const SizedBox(height: 30.0),
-            Row(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: const Text('Ninja ID Card'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ninjaLevel += 1;
+            });
+          },
+          child: const Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
+        ),
+        body: Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
+                const Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/thumb.jpg'),
+                    radius: 40.0,
+                  ),
                 ),
-                const SizedBox(width: 10.0),
-                Text(
-                  'chun.li@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  )
+                Divider(
+                  height: 90.0,
+                  color: Colors.grey[800],
+                ),
+                const Text('NAME',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      letterSpacing: 2.0,
+                    )),
+                const SizedBox(height: 10.0),
+                Text('Chun-Li',
+                    style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2.0,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const SizedBox(height: 30.0),
+                const Text('CURRENT NINJA LEVEL',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      letterSpacing: 2.0,
+                    )),
+                const SizedBox(height: 10.0),
+                Text('$ninjaLevel',
+                    style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2.0,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const SizedBox(height: 30.0),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey[400],
+                    ),
+                    const SizedBox(width: 10.0),
+                    Text('chun.li@gmail.com',
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 18.0,
+                          letterSpacing: 1.0,
+                        ))
+                  ],
                 )
               ],
-            )
-          ],
-        )
-      )
-    );
+            )));
   }
 }
-
-
