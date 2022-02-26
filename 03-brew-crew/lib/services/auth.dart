86322dfa6182f1aa872b2a99da61d6cc1bdaec19
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-26 13:58:37 +0100

    add custom user class and stream

--- 03-brew-crew/lib/services/auth.dart
+++ 03-brew-crew/lib/services/auth.dart
@@ -1,14 +1,25 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:brew_crew/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create MyUser object
+  MyUser? _userFromFirebase(User? user) {
+    return user != null ? MyUser(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<MyUser?> get user {
+    return _auth.authStateChanges().map((User? user) => _userFromFirebase(user));
+  }
 
   // sign in anon
   Future signInAnon() async {
     try {
       UserCredential userCredential = await _auth.signInAnonymously();
       User user = userCredential.user!;
-      return user;
+      return _userFromFirebase(user);
     } catch (e) {
       print(e.toString());
       return null;
--- README.md
+++ README.md
@@ -76,6 +76,8 @@ An app to collect brew preferences, using Firebase.
 
 - setting up Firebase in a Flutter app.
 - creating an authentication class with Firebase Auth.
-- handling anonymous sign in.
+- handling anonymous sign in with a custom user model.
+- listening to auth stream with the 'provider' package.
+
 
 Based on [Flutter & Firebase App Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9j--TKIdkb3ISfRbJeJYQwC) by Shaun Pelling - The Net Ninja (2019).
